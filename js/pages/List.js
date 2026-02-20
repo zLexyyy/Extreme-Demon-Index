9b003b6fb0870c3a197430a7dfdc33caccede036
zLexyyy <vamze15@gmail.com>
2026-02-21 00:49:40 +0200

    New records

--- js/pages/List.js
+++ js/pages/List.js
@@ -124,9 +124,6 @@ export default {
                             <td class="mobile">
                                 <img v-if="record.mobile" :src="'/assets/phone-landscape' + (store.dark ? '-dark' : '') + '.svg'" alt="Mobile">
                             </td>
-                            <td class="hz">
-                                <p>{{ record.hz }}Hz</p>
-                            </td>
                         </tr>
                     </table>
                 </div>
